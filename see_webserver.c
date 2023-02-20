583cef73750db26444aab41feb48a4c999a82893
kathmanducoder <kathmanducoder@gmail.com>
2023-02-20 17:06:33 -0500

    Create socket

--- see_webserver.c
+++ see_webserver.c
@@ -1,3 +1,12 @@
+#include <errno.h>
+#include <stdio.h>
+#include <sys/socket.h>
+
 int main(int argc, char *argv[]) {
-    return 0;
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		perror("Error creating socket.");
+		return 1;
+	}
+	return 0;
 }
