c13f95c0d393d19a0c61fb3de411544724c25844
kathmanducoder <kathmanducoder@gmail.com>
2023-02-28 14:12:32 -0500

    Attach socket to port 8080

--- http_webserv.c
+++ http_webserv.c
@@ -3,16 +3,29 @@
  * HTTP header in its 200 OK response body.
  */
 
+#include <arpa/inet.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 
+#define PORT 8080
+
 int main() {
     int server_fd;
+    struct sockaddr_in server_addr;
 
     if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) != 0) {
+        perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
