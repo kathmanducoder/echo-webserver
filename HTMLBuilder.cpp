e4c1ca793d93786ef0ae992470358ee4505a2d2f
kathmanducoder <kathmanducoder@gmail.com>
2023-03-01 18:40:12 -0500

    Update HtML builder

--- HTMLBuilder.cpp
+++ HTMLBuilder.cpp
@@ -3,10 +3,13 @@
 #include "HTMLBuilder.h"
 
 int HTMLBuilder::build_html(char html[], int html_len, char content[]) {
-    int             html_num_bytes;
+    int             string_index = 0;
 
-    html_num_bytes = snprintf(html, html_len, "%s\n%s\n%s\n", "<html>", content, "</html>");
-    html[html_num_bytes] = '\0';
+    string_index = snprintf(html, html_len, "%s\n%s\n%s\n", "<html>", "<body>", "<ul>");
+    string_index += snprintf(html + string_index, html_len, "%s\n", content);
+    string_index += snprintf(html + string_index, html_len, "%s\n%s\n%s\n", "</ul>", "</body>", "</html>");
+
+    html[string_index] = '\0';
 
     return 0;
 }
