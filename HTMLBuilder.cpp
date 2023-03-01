9e226f87256216cf5872895b0f2152360bea544a
kathmanducoder <kathmanducoder@gmail.com>
2023-03-01 18:49:37 -0500

    Tokenize the http request header based on new line, and add them as an item in a list, also apply some styling

--- HTMLBuilder.cpp
+++ HTMLBuilder.cpp
@@ -4,9 +4,15 @@
 
 int HTMLBuilder::build_html(char html[], int html_len, char content[]) {
     int             string_index = 0;
+    char            *token = NULL;
 
-    string_index = snprintf(html, html_len, "%s\n%s\n%s\n", "<html>", "<body>", "<ul>");
-    string_index += snprintf(html + string_index, html_len, "%s\n", content);
+    string_index = snprintf(html, html_len, "%s\n%s\n%s\n", "<html>", "<body>", "<ul style=\"list-style-type: none;\">");
+
+    token = strtok(content, "\n");
+    while (token != NULL) {
+        string_index += snprintf(html + string_index, html_len, "<li>%s</li>", token);
+        token = strtok(NULL, "\n");
+    }
     string_index += snprintf(html + string_index, html_len, "%s\n%s\n%s\n", "</ul>", "</body>", "</html>");
 
     html[string_index] = '\0';
