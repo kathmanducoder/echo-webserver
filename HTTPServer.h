1cafb976d867e0b801aa9f882aab6413782f37ce
kathmanducoder <kathmanducoder@gmail.com>
2023-03-01 14:21:55 -0500

    Add HTTPServer.h

---
+++ HTTPServer.h
@@ -0,0 +1,18 @@
+#ifndef HTTP_SERVER_H
+#define HTTP_SERVER_H
+
+#include <arpa/inet.h>
+
+class HTTPServer {
+    private:
+        int server_port;
+        int server_fd;
+        struct sockaddr_in server_addr;
+
+    public:
+        HTTPServer(int port);
+        ~HTTPServer();
+        void run();
+};
+
+#endif
